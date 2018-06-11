a2c55bf255a1a9e0630c370f5b1b692d33effdc5
alapha23 <alapha23@gmail.com>
2018-06-12 00:26:15 +0900

    add uwg_example profiling information

--- resources/uwg_example.py
+++ resources/uwg_example.py
@@ -1,5 +1,9 @@
 from UWG import UWG
+import cProfile, pstats, StringIO
+#import re
 
+pr = cProfile.Profile()
+pr.enable()
 # Define the .epw, .uwg filenames to create an UWG object.
 # UWG will look for the .epw file in the UWG/resources/epw folder,
 # and the .uwg file in the UWG/resources/parameters folder.
@@ -9,3 +13,10 @@ param_filename = "initialize_singapore.uwg"         # .uwg file name
 # Initialize the UWG object and run the simulation
 uwg = UWG(epw_filename, param_filename)
 uwg.run()
+
+pr.disable()
+s = StringIO.StringIO()
+sortby = 'cumulative'
+ps = pstats.Stats(pr, stream=s).sort_stats(sortby)
+ps.print_stats()
+print s.getvalue()
